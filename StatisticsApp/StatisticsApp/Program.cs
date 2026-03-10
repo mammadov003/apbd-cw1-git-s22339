96bc5d7603cc16bc01aae7d6db04f4de216b4549
s22339 <kon.ash.2402@gmail.com>
2026-03-10 15:19:04 +0100

    Improve startup formatting

--- StatisticsApp/StatisticsApp/Program.cs
+++ StatisticsApp/StatisticsApp/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Statistics application started successfully.");
+        Console.WriteLine("Statistics application started successfully!");
 
         int[] numbers = { 10, 20, 30, 40, 50 };
 
